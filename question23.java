c72edb32a28a491691acaea078a69976fbb2bf68
NIRAKAR RATH <rathnirakar655@gmail.com>
2026-01-12 00:28:06 +0530

    Add files via upload

--- question23.java
+++ question23.java
@@ -1,7 +1,7 @@
-public class question22 {
+public class question23 {
     public static void main(String[] args) {
         int i,j,n=5;
-        for(i=1;i<=n;i++)
+        for(i=n;i>=1;i--)
         {
             for(j=1;j<=n-i;j++)
             {
